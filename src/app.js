83c16f8057d67b8e5f9469c0b74bbd116bf6fb7c
Yeosujin88 <yeosujin88@gmail.com>
2024-04-22 10:47:06 +0800

    injected more weather data on JS

--- src/app.js
+++ src/app.js
@@ -2,11 +2,32 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data*1000);
 
+  timeElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
+function formatDate(date);
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = [ 
+  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+]
+let day = days[date.getDay()];
+
+if (minutes < 0) {
+  minutes = `0${minutes}`;
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "592f5f724017fabc864648b4d1f2ota3";
 
