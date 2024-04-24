20e72d5a5a4a6e551450c50b1f10e49b6c96f961
Yeosujin88 <yeosujin88@gmail.com>
2024-04-24 15:15:07 +0800

    updated weather forecast html and js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
-   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
 function formatDate(date) {
@@ -54,7 +54,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+
+let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+let forecastHtml = "";
+
+days.forEach(function(day){
+forecastHtml =
+forecastHtml +
+ `<div class="row">
+  <div class="col-2">
+    <div class="weather-forecast-date">
+    ${day}</div>
+    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" width="70">
+    <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">18</span> <span class="weather-forecast-temperature-min">12</span>
+    </div>
+  </div>
+</div>
+`;
+});
+  let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHtml = forecastHtml;
+
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
+
